5813d278b61cc85be6f0cd0145518004019c3d68
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2021-02-15 20:48:10 -0600

    video updated

--- src/data/data.js
+++ src/data/data.js
@@ -13,7 +13,7 @@ const data = [
         img__url:
             'https://images.unsplash.com/photo-1604374168824-4eab4b9d50fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         audio: 'practicas_coorporales.mp4',
-        video__url: 'https://www.youtube.com/embed/HBQFSRypSEc',
+        video__url: 'https://www.youtube.com/embed/gLvm0uRynJ4',
     },
 ];
 
